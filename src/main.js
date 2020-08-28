9351df78bc0867979f576700092407e1000798ff
Rakib Hossain <rakibhoossain@gmail.com>
2020-08-29 00:39:59 +0600

    page init setup

--- src/main.js
+++ src/main.js
@@ -7,6 +7,17 @@ window.$ = window.jQuery = require("jquery");
 import "bootstrap";
 import "font-awesome/css/font-awesome.css";
 import "./assets/style.scss";
+
+import "./assets/css/slick.css";
+import "./assets/css/magnific-popup.css";
+import "./assets/css/font.awesome.css";
+import "./assets/css/ionicons.min.css";
+import "./assets/css/linearicons.css";
+import "./assets/css/animate.css";
+import "./assets/css/jquery-ui.min.css";
+import "./assets/css/slinky.menu.css";
+import "./assets/css/plugins.css";
+
 import "./assets/css/style.css";
 let owl_carousel = require("owl.carousel");
 import "owl.carousel/dist/assets/owl.carousel.min.css";
