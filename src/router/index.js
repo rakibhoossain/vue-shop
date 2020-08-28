1f147b492b211bb127628a4c6c1c368b91e4b887
Rakib Hossain <rakibhoossain@gmail.com>
2020-08-29 01:05:29 +0600

    vuex active css

--- src/router/index.js
+++ src/router/index.js
@@ -20,7 +20,9 @@ const routes = [
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes,
+  linkActiveClass: "active",
+  linkExactActiveClass: "exact-active"
 });
 
 export default router;
