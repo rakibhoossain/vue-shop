e5877ae95e1cc7a7975e0d82ef2f994ef47b194f
Rakib Hossain <rakibhoossain@gmail.com>
2020-08-29 01:26:44 +0600

    sticky menu

--- src/App.vue
+++ src/App.vue
@@ -410,6 +410,16 @@ export default {
     new WOW({ live: false }).init();
     /*--- niceSelect---*/
     $(".select_option").niceSelect();
+
+    /*---stickey menu---*/
+    $(window).on("scroll", function() {
+      var scroll = $(window).scrollTop();
+      if (scroll < 100) {
+        $(".sticky-header").removeClass("sticky");
+      } else {
+        $(".sticky-header").addClass("sticky");
+      }
+    });
   }
 };
 </script>
